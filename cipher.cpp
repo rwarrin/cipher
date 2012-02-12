e311204f7e6c27644b6818ff5a3d7576345b9bbb
Rick Warrington <phial1020@gmail.com>
2012-02-12 10:12:48 -0700

    Argument checking finished.
    Finished checking for proper arguments.

--- cipher.cpp
+++ cipher.cpp
@@ -13,6 +13,23 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+    using namespace std;
+
+    // check arguments
+    if(argc != 3) {
+        cout << "Not enough arguments.\n./cipher -direction <shift amount>\n";
+        cout << "-direction\n\t-e: Encrypt\n\t-d: Decrypt\n";
+        return 1;
+    }
+
+    if(strcmp(argv[1], "-e") == 0)
+        cout << "Encrypt\n";
+    else if(strcmp(argv[1], "-d") == 0)
+        cout << "Decrypt\n";
+    else {
+        cout << "Invalid direction.\n";
+        return 2;
+    }
 
     return 0;
 }
