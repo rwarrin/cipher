3759286cbf53a06bee0eeac998519658ea7001f8
Rick Warrington <phial1020@gmail.com>
2012-02-12 09:52:23 -0700

    Initial commit.

---
+++ cipher.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+ * cipher.cpp
+ *
+ * Rick W
+ * 02/12/2012
+ *
+ * Encrypts a string of text by shifting letters 'shift' amount.
+ *
+ *****************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
